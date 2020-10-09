fc97bfb0e2bde99766e46c410e40b2e5a2e8a226
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-09 17:13:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,5 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |calls|
     if calls.include?(valid_calls)
-      calls
       end
-    end
 end
