085f41cc76a8d3c47e94176abfcc4851b7fe686c
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-09 17:07:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  
+  answer = false 
   planeteer_calls.each do |calls|
-    if calls >
+    if calls > 
 end
 
 def find_valid_calls(planeteer_calls)
